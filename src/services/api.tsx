3ceb001b9111bf3036fc64d8e5618cfa0caff6be
Gabriel Nicácio <gabrielnicacio@MacBook-Pro-de-Antero.local>
2022-06-03 19:32:29 -0300

    feat: service interceptor to authorization

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -4,5 +4,21 @@ const api = axios.create({
   baseURL: 'https://paguru-restaurant-api.herokuapp.com'
 });
 
+api.interceptors.request.use(
+  async config => {
+    const token = localStorage.getItem('@Restaurant:token');
+    if (token !== null) {
+      config.headers = {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json',
+      };
+    }
+    return config;
+  },
+  error => {
+    Promise.reject(error);
+  },
+);
+
 
 export default api;
